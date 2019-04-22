b28ab8f0f768a4d559ad60020fa9352e47257bd7
Martin Chong <martinmanho@gmail.com>
2019-04-21 23:16:47 -0400

    Added day1 progress on the anime.js app

---
+++ animeJS/main.js
@@ -0,0 +1,12 @@
+anime({
+    targets:'div.box.r',
+    translateY: [
+        {value: 250, duration: 1000},
+        {value: 100, duration: 800}
+    ],
+    rotate: {
+        value: '2turn',
+        easing: 'easeInOutSine'
+    }
+
+}); 
