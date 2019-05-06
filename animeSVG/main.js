018f59bfd7ceb4197be8c2ea0facb4a10741b8db
Martin Chong <martinmanho@gmail.com>
2019-05-05 23:58:58 -0400

    Progression in updating loop count for 3rd SVG

--- animeSVG/main.js
+++ animeSVG/main.js
@@ -9,12 +9,23 @@ anime({
   });
 
 var path = anime.path('.path3');
+var loopBegan = 0;
+var loopCompleted = 0;
 anime({
   targets: '.box',
   translateX: path('x'),
   translateY: path('y'),
   rotate: path('angle'),
+  direction: 'alternate',
   easing: 'linear',
   duration: 1000,
-  loop: true
+  loop: true,
+  loopBegin: function(anim) {
+    loopBegan++;
+    beginLogEl.value = 'loop began : ' + loopBegan;
+  },
+  loopComplete: function(anim) {
+    loopCompleted++;
+    completeLogEl.value = 'loop completed : ' + loopCompleted;
+  }
 });
